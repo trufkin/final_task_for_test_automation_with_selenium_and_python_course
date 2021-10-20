82c265e63954f2e89e6d81c897a525b799f615b2
Alexandr Trufkin <alexandrtrufkin@mymac.local>
2021-10-21 01:10:43 +0300

    added base page and main page classes

---
+++ pages/base_page.py
@@ -0,0 +1,10 @@
+
+
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self, browser, url):
+        self.browser.get(self.url)
+
--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,11 @@
-from selenium.webdriver.common.by import By
-
-link = "http://selenium1py.pythonanywhere.com/"
-
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-
+from .pages.main_page import MainPage
+import time
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)
+    page.open(browser, url)
+    time.sleep(5)
+    page.go_to_login_page()
+    time.sleep(5)
+
