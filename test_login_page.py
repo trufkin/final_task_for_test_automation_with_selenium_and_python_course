e2b1d0dd8872f8f1e609531cdaee4e3979d04452
trufkin <trufkin@gmail.com>
2021-10-28 05:33:38 +0300

    removed unnecessary comments as per 4.2.9

--- test_login_page.py
+++ test_login_page.py
@@ -9,3 +9,4 @@ def test_login_page_is_open(browser):
     main_page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.there_should_be_login_page()
+    login_page.there_should_be_register_form()
